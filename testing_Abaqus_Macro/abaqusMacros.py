0f829b439b7c54644c3df9405db44c1b7a6cd4c2
Abdul Rehman <abdulrehmanfayyaz123@gmail.com>
2026-02-09 15:24:36 +0100

    Abaqus Macro testing in commercial license

---
+++ testing_Abaqus_Macro/abaqusMacros.py
@@ -0,0 +1,43 @@
+# -*- coding: mbcs -*-
+# Do not delete the following import lines
+from abaqus import *
+from abaqusConstants import *
+import __main__
+
+def Macro1():
+    import section
+    import regionToolset
+    import displayGroupMdbToolset as dgm
+    import part
+    import material
+    import assembly
+    import step
+    import interaction
+    import load
+    import mesh
+    import optimization
+    import job
+    import sketch
+    import visualization
+    import xyPlot
+    import displayGroupOdbToolset as dgo
+    import connectorBehavior
+    s = mdb.models['Model-1'].ConstrainedSketch(name='__profile__', 
+        sheetSize=200.0)
+    g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
+    s.setPrimaryObject(option=STANDALONE)
+    s.rectangle(point1=(0.0, 0.0), point2=(15.0, 10.0))
+    p = mdb.models['Model-1'].Part(name='Part-1', dimensionality=THREE_D, 
+        type=DEFORMABLE_BODY)
+    p = mdb.models['Model-1'].parts['Part-1']
+    p.BaseSolidExtrude(sketch=s, depth=20.0)
+    s.unsetPrimaryObject()
+    p = mdb.models['Model-1'].parts['Part-1']
+    session.viewports['Viewport: 1'].setValues(displayedObject=p)
+    del mdb.models['Model-1'].sketches['__profile__']
+    session.viewports['Viewport: 1'].view.setValues(nearPlane=35.9521, 
+        farPlane=79.9173, width=63.8453, height=28.6514, viewOffsetX=9.33318, 
+        viewOffsetY=-1.09033)
+    mdb.save()
+
+
